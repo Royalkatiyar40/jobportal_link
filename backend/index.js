1e2f952057dccd98c5ecc484c564923aa4373f4b
Ansh Katiyar <108641849+Royalkatiyar40@users.noreply.github.com>
2024-10-28 02:32:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,7 @@ import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
-
 const app = express();
-
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
